289f62f12e4840eb7cb224355610633524b3449a
Gautam Mehra <node.enthu@gmail.com>
2016-08-30 09:36:50 -0700

    query for food items working with cuisine and diet filters

--- server/controllers/query.js
+++ server/controllers/query.js
@@ -1,23 +1,27 @@
-
 import FoodItem from '../models/foodItem';
 import moment from 'moment';
 
 /*
-* GET /api/query/foodItems
-*/
+ * GET /api/query/foodItems
+ */
 function foodItems(req, res, next) {
     // right now just return the foodItem with its provider
+    let limit = 12;
+    let combinedDietCuisineFilters = req.query["combinedDietCuisineFilters"];
+    let filterspageNum = req.query["filterspageNum"] *12;
+    console.log(combinedDietCuisineFilters,filterspageNum);
     FoodItem
-        .find({organic:true}, 'name placeOrderBy serviceDate deliveryFlag pickUpStartTime pickUpEndTime _creator')
+        .find(combinedDietCuisineFilters, 'name placeOrderBy serviceDate deliveryFlag pickUpStartTime pickUpEndTime _creator')
         .populate('_creator', 'name img pickUpFlag doYouDeliverFlag')
-        .limit(10) 
-        .exec(function(err,foodItems){
+        .skip(filterspageNum)
+        .limit(12)
+        .exec(function(err, foodItems) {
             res.json(foodItems);
         });
 }
 
 function providers(req, res, next) {
-   
+
 }
 
 export default { foodItems, providers };
