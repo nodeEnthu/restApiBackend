d1c75fdbad213ab57db1c40c7cc2db81ecec88bd
Gautam Mehra <node.enthu@gmail.com>
2017-01-12 19:48:56 -0800

    adding email to be editable

--- server/controllers/send-email.js
+++ server/controllers/send-email.js
@@ -1,20 +1,28 @@
 import path from 'path';
 import async from 'async';
 import nodemailer from 'nodemailer';
-import email_templates from 'email-templates'
+import email_templates from 'email-templates';
+import sesTransport from 'nodemailer-ses-transport';
+
 
 let EmailTemplate = email_templates.EmailTemplate;
 let templatesDir = path.resolve(__dirname, '../../');
 let template = new EmailTemplate(path.join(templatesDir, 'order-submit'));
 
 function orderSubmit(req, res) {
-    let transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'autoenthu@gmail.com',
-            pass: 'tennisenthu123'
-        }
-    });
+    // let transporter = nodemailer.createTransport({
+    //     service: 'Gmail',
+    //     auth: {
+    //         user: 'autoenthu@gmail.com',
+    //         pass: 'tennisenthu123'
+    //     }
+    // });
+    let transport = nodemailer.createTransport(sesTransport({
+        "accessKeyId": "AKIAISGDIT6QWWGXAEPA",
+        "secretAccessKey": "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",
+        "region":'us-west-2',
+        "rateLimit": 5 // do not send more than 5 messages in a second 
+    }));
 
     template.render(req.body, function(err, results) {
         let mailOptions = {
@@ -24,7 +32,7 @@ function orderSubmit(req, res) {
             html: results.html, // html body
         };
 
-        transporter.sendMail(mailOptions, function(error, info) {
+        transport.sendMail(mailOptions, function(error, info) {
             if (error) {
                 return res.json({ error: "there was an error " + error });
             } else {
@@ -35,5 +43,4 @@ function orderSubmit(req, res) {
         });
     });
 }
-
 export default { orderSubmit }
