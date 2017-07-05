0bfffdcd3967841cef2f3f730ed227d1ed05a9e5
Gautam Mehra <node.enthu@gmail.com>
2017-07-05 13:26:11 -0700

    chanhes

---
+++ server/controllers/env.js
@@ -0,0 +1,12 @@
+import config from '../../config/env/index'
+
+function envVars(req, res, next) {
+    let envVar = {
+        initialImageUrl: "https://s3-" + config.REGION + ".amazonaws.com/" + config.AWS_BUCKET_NAME + "/",
+        googleLoginId: config.GOOGLE_LOGIN_ID,
+        facebookLoginId: config.FACEBOOK_LOGIN_ID
+    }
+    res.json(envVar);
+}
+
+export default { envVars }
